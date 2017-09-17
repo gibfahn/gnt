23e40189bd6da4d1935696ec1fb7f6345e141783
Gibson Fahnestock <gibfahn@gmail.com>
2017-09-18 00:12:47 +0100

    Remove debugging code
    Now opens a whole page of tabs, and doesn't highlight things red.

--- background.js
+++ background.js
@@ -1,16 +1,5 @@
-console.log('background helloooo')
 browser.runtime.onMessage.addListener(openAllInTabs);
 
 function openAllInTabs(message) {
-  console.log('message:', message)
-  // Just open 5 for testing purposes.
-  // TODO(gib): Actually open all the tabs.
-  let count = 0;
-  message.urls.forEach(url => {
-    if (count < 5) {
-      count++
-      const creating = browser.tabs.create({url})
-      console.log(`creating ${count}`, creating)
-    }
-  })
+  message.urls.forEach(url => browser.tabs.create({url}))
 }
--- button.js
+++ button.js
@@ -1,7 +1,3 @@
-// TODO(gib): remove for actual use.
-// Useful for debugging.
-document.body.style.border = "5px solid red";
-
 const notificationCenter = document.getElementById('notification-center')
 const tabnav = notificationCenter.getElementsByClassName('tabnav')[0]
 
@@ -14,15 +10,12 @@ newButton.className = existingButton.className
 newButton.innerText = 'Open all in tabs'
 newButton.onclick = passUrlsToBackground
 
-console.log('newButton:', newButton)
-
 rightBar.insertBefore(newButton, existingButton)
 
 function passUrlsToBackground(event) {
   const urls = Array.from(
     document.getElementsByClassName('js-navigation-open js-notification-target')
   ).map(a => a.href)
-  console.log('urls:', urls)
   browser.runtime.sendMessage({urls: urls})
   // TODO(gib): when do we reload the tab? Maybe when all the tabs have been opened?
   // window.location.reload(true)
