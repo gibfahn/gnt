1aee03f7b996662c561863731457f8f329d31ff2
Gibson Fahnestock <gibfahn@gmail.com>
2017-09-23 18:20:10 +0100

    1.4.0: support any Github notification page
    - Update URL matching to include anything matching
    `*github*notifications*`.
    - Make script handle there not being a `Mark all as read` button to
    copy.

--- github-notifications-in-tabs/button.js
+++ github-notifications-in-tabs/button.js
@@ -1,22 +1,32 @@
 const notificationCenter = document.getElementById('notification-center')
 const tabnav = notificationCenter.getElementsByClassName('tabnav')[0]
-
 const rightBar = tabnav.getElementsByClassName('float-right')[0]
+
 const existingButton = rightBar.getElementsByClassName('btn-sm')[0]
 
 const newButton = document.createElement('button')
 
-newButton.className = existingButton.className
+let insertBefore
+if (existingButton === undefined) {
+  // There isn't a button class to copy, use last known version.
+  newButton.className = 'btn btn-sm'
+  // Insert at the end.
+  insertBefore = null
+} else {
+  newButton.className = existingButton.className
+  insertBefore = existingButton
+}
+
 newButton.innerText = 'Open all in tabs'
 newButton.onclick = passUrlsToBackground
 
-rightBar.insertBefore(newButton, existingButton)
+rightBar.insertBefore(newButton, insertBefore)
 
 function passUrlsToBackground(event) {
   const urls = Array.from(
     document.getElementsByClassName('js-navigation-open js-notification-target')
   ).map(a => a.href)
   browser.runtime.sendMessage({urls: urls})
-  // TODO(gib): when do we reload the tab? Maybe when all the tabs have been opened?
-  // window.location.reload(true)
+  // TODO(gib): should we reload the tab? Maybe when all the tabs have been
+  // opened? window.location.reload(true)
 }
