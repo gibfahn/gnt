f1cc37e4358025c3e572f73d9a0006dfafca47fd
Gibson Fahnestock <gibfahn@gmail.com>
2018-07-02 11:39:47 +0100

    Update to handle changes in Github html
    The existing buttons are no longer necessarily direct children of the
    tab bar. Specifically the `Mark as Read` button is a <summary> element
    inside a <details> element as it has a confirmation box.

--- github-notifications-in-tabs/button.js
+++ github-notifications-in-tabs/button.js
@@ -1,32 +1,24 @@
-const notificationCenter = document.getElementById('notification-center')
-const tabnav = notificationCenter.getElementsByClassName('tabnav')[0]
-const rightBar = tabnav.getElementsByClassName('float-right')[0]
+const notificationCenter = document.getElementById('notification-center');
+const tabnav = notificationCenter.getElementsByClassName('tabnav')[0];
+const rightBar = tabnav.getElementsByClassName('float-right')[0];
 
-const existingButton = rightBar.getElementsByClassName('btn-sm')[0]
+const newButton = document.createElement('button');
+const existingButton = rightBar.getElementsByClassName('btn-sm')[0];
 
-const newButton = document.createElement('button')
-
-let insertBefore
-if (existingButton === undefined) {
-  // There isn't a button class to copy, use last known version.
-  newButton.className = 'btn btn-sm'
-  // Insert at the end.
-  insertBefore = null
-} else {
-  newButton.className = existingButton.className
-  insertBefore = existingButton
-}
+// If there isn't a button class to copy, use last known class.
+newButton.className = existingButton !== undefined ? existingButton.className : 'btn btn-sm';
 
 newButton.innerText = 'Open all in tabs'
-newButton.onclick = passUrlsToBackground
+newButton.onclick = passUrlsToBackground;
 
-rightBar.insertBefore(newButton, insertBefore)
+// Insert at the end if no existing elements.
+rightBar.insertBefore(newButton, rightBar.firstChild);
 
 function passUrlsToBackground(event) {
   const urls = Array.from(
     document.getElementsByClassName('js-navigation-open js-notification-target')
-  ).map(a => a.href)
-  browser.runtime.sendMessage({urls: urls})
-  // TODO(gib): should we reload the tab? Maybe when all the tabs have been
-  // opened? window.location.reload(true)
+  ).map(a => a.href);
+  browser.runtime.sendMessage({urls: urls});
+  // TODO(gib): should we reload the tab? Maybe when all the tabs have been opened?
+  // window.location.reload(true)
 }
