4b802c373fbd9268066c6d5f345661355c8851ce
Gibson Fahnestock <gibfahn@gmail.com>
2020-08-06 18:57:49 +0100

    fix(background): fix typo in URL passing, rename files
    Common file prefix makes things less likely to conflict.

--- github-notifications-in-tabs/gnt-background.js
+++ github-notifications-in-tabs/gnt-background.js
@@ -1,6 +1,6 @@
 browser.runtime.onMessage.addListener(({urls}) => {
   if (Array.isArray(urls)) {
-    for (const url of message.urls) {
+    for (const url of urls) {
       browser.tabs.create({url: url});
     }
     return Promise.resolve("URLs opened.");
